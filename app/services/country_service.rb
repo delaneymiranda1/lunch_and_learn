dde1846504fe3a6d1ba6d50d873aa1479b865e4c
Miranda Delaney <delaney.miranda1@gmail.com>
2024-03-04 10:55:53 -0700

    add ability to retrieve capital

--- app/services/country_service.rb
+++ app/services/country_service.rb
@@ -1,15 +1,24 @@
 class CountryService
 
   def get_url(url)
-    response = Faraday.get(url)
+    response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
 
   def get_countries
-    response = get_url("https://restcountries.com/v3.1/all")
+    response = get_url("all")
   end
 
   def get_random_country
     get_countries.sample[:name][:common]
+  end
+
+  def get_capital(country)
+    data = get_url("name/#{country}?fullText=true")
+    data.first[:capital].first
+  end
+
+  def conn
+    Faraday.new(url: "https://restcountries.com/v3.1/")
   end
 end
