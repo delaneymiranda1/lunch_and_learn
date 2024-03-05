651d32f71ea1e26034b98d4f5735bfcda8fc1571
Miranda Delaney <delaney.miranda1@gmail.com>
2024-03-05 14:47:50 -0700

    Add sessions create action and route

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
+      resources :sessions, only: [:create]
       resources :recipes, only: [:index]
       get '/learning_resources', to: 'countries#show', as: 'country'
     end
