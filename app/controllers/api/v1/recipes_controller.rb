0e1159153d3dbb1cd5cf6ecc7e6b15dff0518958
Miranda Delaney <delaney.miranda1@gmail.com>
2024-03-04 18:16:06 -0700

    strange error fix

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::RecipesController < ApplicationController
 
   def index
     country = params[:country]
-    @recipes = RecipesFacade.new.search_by_country(country)
-    render json: RecipesSerializer.new(@recipes).serializable_hash.to_json
+    recipes = RecipesFacade.new.search_by_country(country)
+    render json: RecipesSerializer.new(recipes).serializable_hash.to_json
   end
 end
