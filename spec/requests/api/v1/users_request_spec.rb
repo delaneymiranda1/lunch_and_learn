f38ca68b0e40715ee18e3e913788d0622f2cbd2e
Miranda Delaney <delaney.miranda1@gmail.com>
2024-03-05 13:48:59 -0700

    Testing partially done for user

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "Users" type: :request do
+RSpec.describe "Users", type: :request do
 
   describe "POST /api/v1/users" do
     it "creates a new user in the database", :vcr do
