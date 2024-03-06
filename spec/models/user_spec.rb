5dbf177cac823bd44ab0d3548bbccb7834a2604c
Miranda Delaney <delaney.miranda1@gmail.com>
2024-03-05 17:26:01 -0700

    US 5 complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
+    it { should have_many(:favorites) }
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password_digest) }
